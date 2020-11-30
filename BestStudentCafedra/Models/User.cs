e07166b5564c7c61ac9ffd95c558b69d89929ed1
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2020-11-30 22:55:25 +0300

    Added user model

---
+++ BestStudentCafedra/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class User: IdentityUser
+    {
+        public int? SubjectAreaId { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string MiddleName { get; set; }
+    }
+}
