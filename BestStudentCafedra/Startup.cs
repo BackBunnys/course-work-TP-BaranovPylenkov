32ae8a483e3c70991a5c43e2c4839f867085a7f4
Pylenkov Roman <solodov1994roma@gmail.com>
2020-12-01 21:49:19 +0300

    Configured connection to remote MySQL Server

--- BestStudentCafedra/Startup.cs
+++ BestStudentCafedra/Startup.cs
@@ -31,6 +31,9 @@ namespace BestStudentCafedra
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<SubjectAreaDbContext>(options => 
+                options.UseMySql(
+                    Configuration.GetConnectionString("SubjectAreaConnection"), ServerVersion.FromString("8.0.22-mysql")));
             services.AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
@@ -42,7 +45,6 @@ namespace BestStudentCafedra
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
