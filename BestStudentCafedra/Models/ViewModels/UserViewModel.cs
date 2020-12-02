2c08440077b6c00c1e629788ac86bda206f21a87
Pylenkov Roman <solodov1994roma@gmail.com>
2020-12-02 03:56:57 +0300

    Added ModelView class and Index method to controller to show list of users

---
+++ BestStudentCafedra/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestStudentCafedra.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        [Display(Name = "Почта")]
+        public string Email { get; set; }
+        [Display(Name = "Подтверждён")]
+        public bool IsConfirmed { get; set; }
+        [Display(Name = "Роли")]
+        public IList<String> Roles { get; set; }
+        [Display(Name = "Имя")]
+        public string FullName { get; set; }
+    }
+}
