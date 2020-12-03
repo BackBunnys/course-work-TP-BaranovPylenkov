d79f8df623f49afa7b51bfb88308662c2334e160
Pylenkov Roman <solodov1994roma@gmail.com>
2020-12-04 01:14:55 +0300

    Added Annotations to model class

--- BestStudentCafedra/Models/ProposedTopic.cs
+++ BestStudentCafedra/Models/ProposedTopic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,6 +9,9 @@ namespace BestStudentCafedra.Models
     public partial class ProposedTopic
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Введите тему")]
+        [MinLength(10, ErrorMessage = "Тема должна быть длиной больше 10 символов")]
+        [Display(Name = "Название")]
         public string Name { get; set; }
     }
 }
