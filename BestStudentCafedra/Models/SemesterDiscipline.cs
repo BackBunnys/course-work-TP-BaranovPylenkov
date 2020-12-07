f985b88fc7af7b4f95344bd818f87c73f839ecaa
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2020-12-08 02:23:22 +0300

    Added annotation for model semester discipline

--- BestStudentCafedra/Models/SemesterDiscipline.cs
+++ BestStudentCafedra/Models/SemesterDiscipline.cs
@@ -16,12 +16,15 @@ namespace BestStudentCafedra.Models
         public int Id { get; set; }
         public int DisciplineId { get; set; }
         [Display(Name = "Курс")]
+        [Required(ErrorMessage = "Не указан учебный курс")]
+        [Range(1, 6, ErrorMessage = "Курс должен быть в диапозоне от 1 до 6")]
         public int Year { get; set; }
         [Display(Name = "Семестр")]
+        [Required(ErrorMessage = "Не указан семестр")]
+        [Range(1, 12, ErrorMessage = "Семестр должен быть в диапозоне от 1 до 12")]
         public int Semester { get; set; }
         [UIHint("Enum")]
         [EnumDataType(typeof(ControlType))]
-        [Required(ErrorMessage = "Please select supporter tier")]
         [Display(Name = "Тип контроля")]
         public ControlType ControlType { get; set; }
 
