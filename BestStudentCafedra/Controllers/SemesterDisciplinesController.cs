355791760d1548d2a2cef959338b3cfffbe2a9f0
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2020-12-08 13:41:07 +0300

    Added return url for semester discipline CRUD

--- BestStudentCafedra/Controllers/SemesterDisciplinesController.cs
+++ BestStudentCafedra/Controllers/SemesterDisciplinesController.cs
@@ -78,14 +78,14 @@ namespace BestStudentCafedra.Controllers
                 }
                 else
                 {
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Discipline");
                 }
             }
             return View(semesterDiscipline);
         }
 
         // GET: SemesterDisciplines/Edit/5
-        public async Task<IActionResult> Edit(int? id, string returnUrl)
+        public async Task<IActionResult> Edit(int? id, int disciplineId, string returnUrl)
         {
             if (id == null)
             {
@@ -97,7 +97,8 @@ namespace BestStudentCafedra.Controllers
             {
                 return NotFound();
             }
-            ViewData["DisciplineId"] = new SelectList(_context.Disciplines, "Id", "Name", semesterDiscipline.DisciplineId);
+
+            ViewData["returnUrl"] = returnUrl;
             return View(semesterDiscipline);
         }
 
@@ -106,7 +107,7 @@ namespace BestStudentCafedra.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,DisciplineId,Year,Semester,ControlType")] SemesterDiscipline semesterDiscipline)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DisciplineId,Year,Semester,ControlType")] SemesterDiscipline semesterDiscipline, string returnUrl)
         {
             if (id != semesterDiscipline.Id)
             {
@@ -131,7 +132,15 @@ namespace BestStudentCafedra.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+
+                if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Discipline");
+                }
             }
             ViewData["DisciplineId"] = new SelectList(_context.Disciplines, "Id", "Name", semesterDiscipline.DisciplineId);
             return View(semesterDiscipline);
@@ -154,7 +163,6 @@ namespace BestStudentCafedra.Controllers
             }
 
             ViewData["returnUrl"] = returnUrl;
-
             return View(semesterDiscipline);
         }
 
