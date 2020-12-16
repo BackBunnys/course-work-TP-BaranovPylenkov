aa0b2f96d9771239fb00954e41f1b149ffca487a
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2020-12-17 00:26:24 +0300

    Added annotation for model academic group

--- BestStudentCafedra/Models/AcademicGroup.cs
+++ BestStudentCafedra/Models/AcademicGroup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -15,11 +16,17 @@ namespace BestStudentCafedra.Models
 
         public int Id { get; set; }
         public string SpecialtyId { get; set; }
+        [Required(ErrorMessage = "Не указано название группы")]
+        [Display(Name = "Название группы")]
         public string Name { get; set; }
+        [Range(1000, 9999, ErrorMessage = "Год формирования группы должен быть в диапозоне от 1000 до 9999")]
+        [Display(Name = "Год формирования группы")]
         public int? FormationYear { get; set; }
 
+        [Display(Name = "Специальность")]
         public virtual Specialty Specialty { get; set; }
         public virtual ICollection<SchedulePlan> SchedulePlans { get; set; }
+        [Display(Name = "Студенты")]
         public virtual ICollection<Student> Students { get; set; }
     }
 }
