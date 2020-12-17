45b22d9756575adce7413953e59b01135f06074e
Pylenkov Roman <solodov1994roma@gmail.com>
2020-12-18 01:31:50 +0300

    Added auto generated controller

---
+++ BestStudentCafedra/Controllers/EventsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BestStudentCafedra.Data;
+using BestStudentCafedra.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BestStudentCafedra.Controllers
+{
+    [Authorize(Roles="teacher")]
+    public class EventsController : Controller
+    {
+        private readonly SubjectAreaDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public EventsController(SubjectAreaDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Events
+        public async Task<IActionResult> Index()
+        {
+            var subjectAreaDbContext = _context.Events.Include(x => x.ResponsibleTeacher).Include(x => x.SchedulePlan);
+            return View(await subjectAreaDbContext.ToListAsync());
+        }
+
+        // GET: Events/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .Include(x => x.ResponsibleTeacher)
+                .Include(x => x.SchedulePlan)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public IActionResult Create()
+        {
+            ViewData["ResponsibleTeacherId"] = new SelectList(_context.Teachers, "Id", "FullName");
+            ViewData["SchedulePlanId"] = new SelectList(_context.SchedulePlans, "Id", "Id");
+            return View();
+        }
+
+        // POST: Events/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SchedulePlanId,EventDescription,Date,Class,ResponsibleTeacherId")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ResponsibleTeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", @event.ResponsibleTeacherId);
+            ViewData["SchedulePlanId"] = new SelectList(_context.SchedulePlans, "Id", "Id", @event.SchedulePlanId);
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+            ViewData["ResponsibleTeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", @event.ResponsibleTeacherId);
+            ViewData["SchedulePlanId"] = new SelectList(_context.SchedulePlans, "Id", "Id", @event.SchedulePlanId);
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SchedulePlanId,EventDescription,Date,Class,ResponsibleTeacherId")] Event @event)
+        {
+            if (id != @event.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(@event.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ResponsibleTeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", @event.ResponsibleTeacherId);
+            ViewData["SchedulePlanId"] = new SelectList(_context.SchedulePlans, "Id", "Id", @event.SchedulePlanId);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .Include(x => x.ResponsibleTeacher)
+                .Include(x => x.SchedulePlan)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @event = await _context.Events.FindAsync(id);
+            _context.Events.Remove(@event);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.Id == id);
+        }
+    }
+}
