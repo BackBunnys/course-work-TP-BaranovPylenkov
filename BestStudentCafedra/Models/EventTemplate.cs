8baccfa46d1cf40a6c5970518bec713886a01465
Pylenkov Roman <solodov1994roma@gmail.com>
2020-12-17 05:03:58 +0300

    Added annotations to model class

--- BestStudentCafedra/Models/EventTemplate.cs
+++ BestStudentCafedra/Models/EventTemplate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,7 +9,11 @@ namespace BestStudentCafedra.Models
     public partial class EventTemplate
     {
         public int Id { get; set; }
+        [Display(Name = "№ п/п")]
         public int SequentialNumber { get; set; }
+        [Display(Name = "Описание мероприятия")]
+        [Required(ErrorMessage = "Не указано содержание мероприятия")]
+        [StringLength(maximumLength: 150, MinimumLength = 5, ErrorMessage = "Описание мероприятия должно содержать от 5 до 150 символов")]
         public string Description { get; set; }
     }
 }
