d7c8ff8df0c53acae5c5c055ba41b6ee246a9a86
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2020-12-17 12:20:29 +0300

    Added auto generated controllers and views for activities

---
+++ BestStudentCafedra/Controllers/ActivitiesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BestStudentCafedra.Data;
+using BestStudentCafedra.Models;
+
+namespace BestStudentCafedra.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        private readonly SubjectAreaDbContext _context;
+
+        public ActivitiesController(SubjectAreaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Activities
+        public async Task<IActionResult> Index()
+        {
+            var subjectAreaDbContext = _context.Activities.Include(a => a.SemesterDiscipline).Include(a => a.Type);
+            return View(await subjectAreaDbContext.ToListAsync());
+        }
+
+        // GET: Activities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activities
+                .Include(a => a.SemesterDiscipline)
+                .Include(a => a.Type)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        // GET: Activities/Create
+        public IActionResult Create()
+        {
+            ViewData["DisciplineId"] = new SelectList(_context.SemesterDiscipline, "Id", "Id");
+            ViewData["TypeId"] = new SelectList(_context.ActivityTypes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Activities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TypeId,DisciplineId,Number,Title,MaxPoints")] Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(activity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DisciplineId"] = new SelectList(_context.SemesterDiscipline, "Id", "Id", activity.DisciplineId);
+            ViewData["TypeId"] = new SelectList(_context.ActivityTypes, "Id", "Name", activity.TypeId);
+            return View(activity);
+        }
+
+        // GET: Activities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activities.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+            ViewData["DisciplineId"] = new SelectList(_context.SemesterDiscipline, "Id", "Id", activity.DisciplineId);
+            ViewData["TypeId"] = new SelectList(_context.ActivityTypes, "Id", "Name", activity.TypeId);
+            return View(activity);
+        }
+
+        // POST: Activities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TypeId,DisciplineId,Number,Title,MaxPoints")] Activity activity)
+        {
+            if (id != activity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(activity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActivityExists(activity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DisciplineId"] = new SelectList(_context.SemesterDiscipline, "Id", "Id", activity.DisciplineId);
+            ViewData["TypeId"] = new SelectList(_context.ActivityTypes, "Id", "Name", activity.TypeId);
+            return View(activity);
+        }
+
+        // GET: Activities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activities
+                .Include(a => a.SemesterDiscipline)
+                .Include(a => a.Type)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        // POST: Activities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            _context.Activities.Remove(activity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActivityExists(int id)
+        {
+            return _context.Activities.Any(e => e.Id == id);
+        }
+    }
+}
