c4cfd4d2c6b54f454008b03dbcdfc18d6bb139f2
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2020-12-22 09:31:36 +0300

    Translated activity protection model

--- BestStudentCafedra/Models/ActivityProtection.cs
+++ BestStudentCafedra/Models/ActivityProtection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -10,7 +11,9 @@ namespace BestStudentCafedra.Models
         public int Id { get; set; }
         public int StudentId { get; set; }
         public int ActivityId { get; set; }
+        [Display(Name = "Время защиты")]
         public DateTime ProtectionDate { get; set; }
+        [Display(Name = "Баллы")]
         public int Points { get; set; }
 
         public virtual Activity Activity { get; set; }
