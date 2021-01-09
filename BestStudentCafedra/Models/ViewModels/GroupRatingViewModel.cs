496936d1b495a91e2a8022c7dbbd7daa911275f6
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2021-01-09 23:00:52 +0300

    Added group rating view model

---
+++ BestStudentCafedra/Models/ViewModels/GroupRatingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestStudentCafedra.Models.ViewModels
+{
+    public class GroupRatingViewModel
+    {
+        public AcademicGroup Group;
+        public IEnumerable<RatingControl> RatingControls;
+    }
+}
