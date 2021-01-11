5c6128cab79287bf95ef5c28f05c8d87d88684e9
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2021-01-11 23:11:59 +0300

    Added generate and download current rating exel table action in controller

--- BestStudentCafedra/Controllers/RatingControlsController.cs
+++ BestStudentCafedra/Controllers/RatingControlsController.cs
@@ -15,6 +15,7 @@ using System.Globalization;
 using System.ComponentModel;
 using System.Reflection;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace BestStudentCafedra.Controllers
 {
@@ -321,6 +322,157 @@ namespace BestStudentCafedra.Controllers
                     return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"{ratingControl.AcademicGroup.Name} рейтинг №{ratingControl.Number}.xlsx");
                 }
             }
+        }
+
+        public async Task<IActionResult> DownloadCurrentRating(int groupId, int disciplineId)
+        {
+            var ratingControls = await _context.RatingControls
+                .Include(x => x.StudentRatings)
+                .Where(x => x.SemesterDisciplineId == disciplineId && x.GroupId == groupId)
+                .OrderBy(x => x.Number)
+                .ToListAsync();
+
+            var group = await _context.AcademicGroups
+                .Include(x => x.Specialty)
+                .Include(x => x.Students.OrderBy(y => y.FullName))
+                    .ThenInclude(x => x.ActivityProtections
+                        .Where(y => _context.Activities
+                            .Where(z => z.SemesterDisciplineId == disciplineId)
+                            .Any(h => y.ActivityId == h.Id)))
+                .FirstOrDefaultAsync(x => x.Id == groupId);
+
+            var semesterDiscipline = await _context.SemesterDiscipline
+                .Include(x => x.Discipline)
+                    .ThenInclude(y => y.TeacherDisciplines)
+                        .ThenInclude(z => z.Teacher)
+                .Include(x => x.Activities)
+                    .ThenInclude(y => y.Type)
+                .FirstOrDefaultAsync(x => x.Id == disciplineId);
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Текущий рейтинг контроль");
+                var currentRow = 1;
+
+                worksheet.Cell(currentRow, 1).Value = "гр.";
+                worksheet.Cell(currentRow, 2).Value = group.Name; worksheet.Cell(currentRow, 2).Style.Fill.BackgroundColor = XLColor.FromArgb(254, 0, 254);
+                worksheet.Cell(currentRow, 2).Style.Font.Bold = true;
+                worksheet.Cell(currentRow, 3).Value = "Расчетная ведомость рейтинга:";
+                currentRow++;
+
+                worksheet.Cell(currentRow, 1).Value = "сп.";
+                worksheet.Cell(currentRow, 2).Value = group.Specialty.Name; worksheet.Cell(currentRow, 2).Style.Fill.BackgroundColor = XLColor.FromArgb(254, 0, 254);
+
+                var currentCol = 3;
+                var r = new Random();
+                foreach (var activityType in semesterDiscipline.Activities.Select(x => x.Type).Distinct())
+                {
+                    var length = semesterDiscipline.Activities.Where(x => x.TypeId == activityType.Id).Count() - 1;
+                    worksheet.Range(worksheet.Cell(currentRow, currentCol), worksheet.Cell(currentRow, currentCol + length)).Merge();
+                    worksheet.Cell(currentRow, currentCol).Value = Abbreviaturization(activityType.Name);
+                    worksheet.Cell(currentRow, currentCol).Style.Font.Bold = true;
+                    worksheet.Cell(currentRow, currentCol).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                    worksheet.Range(worksheet.Cell(currentRow + 1, currentCol), worksheet.Cell(currentRow + 1 + group.Students.Count(), currentCol + length)).Style.Fill.BackgroundColor = XLColor.FromArgb(100 + r.Next(155), 100 + r.Next(155), 100 + r.Next(155));
+                    currentCol += length + 1;
+                }
+                currentRow++;
+
+                worksheet.Cell(currentRow, 1).Value = "№"; worksheet.Cell(currentRow, 1).Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                worksheet.Cell(currentRow, 2).Value = "Ф.И.О. студента"; worksheet.Cell(currentRow, 2).Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+
+                currentCol = 3;
+                foreach (var item in semesterDiscipline.Activities)
+                {
+                    worksheet.Cell(currentRow, currentCol).Value = item.Number;
+                    worksheet.Cell(currentRow, currentCol).Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                    worksheet.Column(currentCol).Width = 4;
+                    currentCol++;
+                }
+
+                worksheet.Cell(currentRow, currentCol).Value = "Итого";
+                worksheet.Range(worksheet.Cell(currentRow + 1, currentCol), worksheet.Cell(currentRow + 1 + group.Students.Count(), currentCol)).Style.Fill.BackgroundColor = XLColor.FromArgb(100 + r.Next(155), 100 + r.Next(155), 100 + r.Next(155));
+                worksheet.Column(currentCol).AdjustToContents();
+                currentCol++;
+
+                foreach (var item in ratingControls)
+                {
+                    worksheet.Cell(currentRow - 1, currentCol).Value = item.Number + " рейтинг";
+                    worksheet.Cell(currentRow - 1, currentCol).Style.Font.FontSize = 8;
+                    worksheet.Cell(currentRow, currentCol).Value = item.CompletionDate.Date;
+                    worksheet.Cell(currentRow, currentCol).Style.Alignment.TextRotation = 90;
+                    worksheet.Cell(currentRow, currentCol).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                    worksheet.Cell(currentRow, currentCol).Style.Font.FontSize = 9;
+                    worksheet.Cell(currentRow, currentCol).Style.Fill.BackgroundColor = XLColor.FromArgb(254, 0, 254);
+                    worksheet.Cell(currentRow, currentCol).Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                    worksheet.Column(currentCol).Width = 5;
+                    currentCol++;
+                }
+
+                worksheet.Row(currentRow).Height = 50;
+                currentRow++;
+
+                var i = 1;
+                foreach (var student in group.Students)
+                {
+                    worksheet.Cell(currentRow, 1).Value = i++;
+                    worksheet.Cell(currentRow, 2).Value = student.FullName;
+                    worksheet.Cell(currentRow, 2).Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                    currentCol = 3;
+                    foreach (var activity in semesterDiscipline.Activities)
+                    {
+                        ActivityProtection activityProtection = student.ActivityProtections.FirstOrDefault(x => x.Activity == activity);
+                        if (activityProtection != null)
+                        {
+                            worksheet.Cell(currentRow, currentCol).Value = activityProtection.Points;
+                        }
+                        currentCol++;
+                    }
+
+                    worksheet.Cell(currentRow, currentCol).FormulaA1 = $"SUM({worksheet.Cell(currentRow, currentCol - 1).Address}:{worksheet.Cell(currentRow, currentCol - semesterDiscipline.Activities.Count).Address})"; currentCol++;
+                    foreach (var ratingControl in ratingControls)
+                    {
+                        StudentRating studentRating = ratingControl.StudentRatings.FirstOrDefault(x => x.StudentId == student.GradebookNumber);
+                        if (studentRating != null)
+                        {
+                            worksheet.Cell(currentRow, currentCol).Value = studentRating.Points;
+                        }
+                        currentCol++;
+                    }
+                    currentRow++;
+                }
+
+                worksheet.Row(currentRow).Style.Fill.BackgroundColor = XLColor.FromArgb(51, 51, 51); currentRow++;
+                currentRow++;
+
+                worksheet.Cell(currentRow, 2).Value = "Преподаватель"; 
+                worksheet.Cell(currentRow, 3).Value = semesterDiscipline.Discipline.TeacherDisciplines.FirstOrDefault(x => x.DisciplineId == semesterDiscipline.DisciplineId).Teacher.FullName;
+
+                worksheet.Column(1).AdjustToContents();
+                worksheet.Column(2).AdjustToContents();
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", group.Name + ".xlsx");
+                }
+            }
+        }
+
+        public string Abbreviaturization(string sentence)
+        {
+            string[] subs = Regex.Replace(sentence, @"\s+", " ").Split(' ');
+            var builder = new StringBuilder();
+            foreach(var word in subs)
+            {
+                if (word[0] != ' ')
+                {
+                    builder.Append(char.ToUpper(word[0]));
+                }
+            }
+            return builder.ToString();
         }
 
         public string GetDisplayName(object obj, string propertyName)
