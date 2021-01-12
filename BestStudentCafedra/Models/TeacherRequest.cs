921c6375bfb32df499b1cce0225c0efdbf32a8ef
Pylenkov Roman <solodov1994roma@gmail.com>
2021-01-12 18:24:03 +0300

    Added teacher request class

---
+++ BestStudentCafedra/Models/TeacherRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestStudentCafedra.Models
+{
+    public class TeacherRequest: Request
+    {
+        public int Id { get; set; }
+        public int GraduationWorkId { get; set; }
+        public int TeacherId { get; set; }
+        public RequestType RequestType { get; set; }
+
+        public override void Approve()
+        {
+            base.Approve();
+            if (RequestType == RequestType.ADVISER)
+                GraduationWork.ScientificAdviserId = TeacherId;
+            else if (RequestType == RequestType.REVIEWER)
+                GraduationWork.ReviewerId = TeacherId;
+        }
+
+        public virtual Teacher Teacher { get; set; }
+        public virtual GraduationWork GraduationWork { get; set; }
+    }
+
+    public enum RequestType { ADVISER, REVIEWER };
+}
