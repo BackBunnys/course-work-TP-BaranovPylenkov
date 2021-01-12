5dd7a8f42bff527abc4a82930367c3926d7f04d3
Pylenkov Roman <solodov1994roma@gmail.com>
2021-01-12 18:19:56 +0300

    Added base request class

---
+++ BestStudentCafedra/Models/Request.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestStudentCafedra.Models
+{
+    public abstract class Request
+    {
+        public Status? Status { get; set; }
+        public string RejectReason { get; set; }
+        public DateTime CreatingDate { get; set; }
+        public DateTime ResponseDate { get; set; }
+
+        public Request()
+        {
+            CreatingDate = DateTime.Now;
+        }
+
+        public virtual void Approve()
+        {
+            Status = Models.Status.APPROVED;
+            ResponseDate = DateTime.Now;
+        }
+
+        public void Reject(string reason)
+        {
+            Status = Models.Status.REJECTED;
+            RejectReason = reason;
+            ResponseDate = DateTime.Now;
+        }
+    }
+
+    public enum Status { APPROVED, REJECTED };
+}
