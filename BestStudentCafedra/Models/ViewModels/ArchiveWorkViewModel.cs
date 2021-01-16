53fce3e738006bd0cd625406deea30977f8348f9
Pylenkov Roman <solodov1994roma@gmail.com>
2021-01-17 02:04:41 +0300

    Added view model for archiving graduation work

---
+++ BestStudentCafedra/Models/ViewModels/ArchiveWorkViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BestStudentCafedra.Models.ViewModels
+{
+    public class ArchiveWorkViewModel
+    {
+        public int GraduationWorkId { get; set; }
+        [Display(Name = "Дата сдачи")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Не указана дата сдачи")]
+        public DateTime ArchievedDate { get; set; }
+        [Display(Name = "Результат")]
+        [Required(ErrorMessage = "Не указан результат работы")]
+        public int Result { get; set; }
+
+        public GraduationWork GraduationWork { get; set; }
+    }
+}
