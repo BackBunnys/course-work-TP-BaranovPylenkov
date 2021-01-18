d27111a9a4aafdcc9ab0ce4de2fd6217b313d109
Pylenkov Roman <solodov1994roma@gmail.com>
2021-01-18 03:05:53 +0300

    Added auto-generated controller

---
+++ BestStudentCafedra/Controllers/TeacherRequestsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BestStudentCafedra.Data;
+using BestStudentCafedra.Models;
+
+namespace BestStudentCafedra.Controllers
+{
+    public class TeacherRequestsController : Controller
+    {
+        private readonly SubjectAreaDbContext _context;
+
+        public TeacherRequestsController(SubjectAreaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TeacherRequests
+        public async Task<IActionResult> Index()
+        {
+            var subjectAreaDbContext = _context.TeacherRequests.Include(t => t.GraduationWork).Include(t => t.Teacher);
+            return View(await subjectAreaDbContext.ToListAsync());
+        }
+
+        // GET: TeacherRequests/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacherRequest = await _context.TeacherRequests
+                .Include(t => t.GraduationWork)
+                .Include(t => t.Teacher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (teacherRequest == null)
+            {
+                return NotFound();
+            }
+
+            return View(teacherRequest);
+        }
+
+        // GET: TeacherRequests/Create
+        public IActionResult Create()
+        {
+            ViewData["GraduationWorkId"] = new SelectList(_context.GraduationWorks, "Id", "Id");
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "FullName");
+            return View();
+        }
+
+        // POST: TeacherRequests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,GraduationWorkId,TeacherId,Motivation,RequestType,Status,RejectReason,CreatingDate,ResponseDate,ResponsePersonName")] TeacherRequest teacherRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(teacherRequest);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GraduationWorkId"] = new SelectList(_context.GraduationWorks, "Id", "Id", teacherRequest.GraduationWorkId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", teacherRequest.TeacherId);
+            return View(teacherRequest);
+        }
+
+        // GET: TeacherRequests/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacherRequest = await _context.TeacherRequests.FindAsync(id);
+            if (teacherRequest == null)
+            {
+                return NotFound();
+            }
+            ViewData["GraduationWorkId"] = new SelectList(_context.GraduationWorks, "Id", "Id", teacherRequest.GraduationWorkId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", teacherRequest.TeacherId);
+            return View(teacherRequest);
+        }
+
+        // POST: TeacherRequests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,GraduationWorkId,TeacherId,Motivation,RequestType,Status,RejectReason,CreatingDate,ResponseDate,ResponsePersonName")] TeacherRequest teacherRequest)
+        {
+            if (id != teacherRequest.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(teacherRequest);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeacherRequestExists(teacherRequest.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GraduationWorkId"] = new SelectList(_context.GraduationWorks, "Id", "Id", teacherRequest.GraduationWorkId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "FullName", teacherRequest.TeacherId);
+            return View(teacherRequest);
+        }
+
+        // GET: TeacherRequests/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teacherRequest = await _context.TeacherRequests
+                .Include(t => t.GraduationWork)
+                .Include(t => t.Teacher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (teacherRequest == null)
+            {
+                return NotFound();
+            }
+
+            return View(teacherRequest);
+        }
+
+        // POST: TeacherRequests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var teacherRequest = await _context.TeacherRequests.FindAsync(id);
+            _context.TeacherRequests.Remove(teacherRequest);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeacherRequestExists(int id)
+        {
+            return _context.TeacherRequests.Any(e => e.Id == id);
+        }
+    }
+}
