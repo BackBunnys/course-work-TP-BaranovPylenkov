9f14642ab483ea67d9d41e37e83d4829215f5dd1
Pylenkov Roman <solodov1994roma@gmail.com>
2021-01-20 22:52:43 +0300

    Added EmailOptions class and rewrited EmailService according to new options class

--- BestStudentCafedra/Services/Messages/Email/EmailService.cs
+++ BestStudentCafedra/Services/Messages/Email/EmailService.cs
@@ -1,4 +1,5 @@
 ﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
 using MimeKit;
 using System;
 using System.Collections.Generic;
@@ -7,13 +8,31 @@ using System.Threading.Tasks;
 
 namespace BestStudentCafedra.Services.Messages.Email
 {
+    public class EmailOptions
+    {
+        public const string Email = "Email";
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool SSL { get; set; }
+    }
+
     public class EmailService: EmailSender
     {
+        private readonly EmailOptions _emailOptions;
+        public EmailService(IOptions<EmailOptions> emailOptions)
+        {
+            _emailOptions = emailOptions.Value;
+        }
+
         public override async Task SendEmailAsync(List<string> adresses, EmailMessage message)
         {
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress("Университет", "login@yandex.ru"));
+            emailMessage.From.Add(new MailboxAddress(_emailOptions.Name, _emailOptions.Address));
             emailMessage.To.AddRange(adresses.Select(x => new MailboxAddress("", x)).ToList());
             emailMessage.Subject = message.Subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
@@ -23,8 +42,8 @@ namespace BestStudentCafedra.Services.Messages.Email
 
             using (var client = new SmtpClient())
             {
-                await client.ConnectAsync("smtp.yandex.ru", 25, false);
-                await client.AuthenticateAsync("login@yandex.ru", "password");
+                await client.ConnectAsync(_emailOptions.Host, _emailOptions.Port, _emailOptions.SSL);
+                await client.AuthenticateAsync(_emailOptions.Address, _emailOptions.Password);
                 await client.SendAsync(emailMessage);
 
                 await client.DisconnectAsync(true);
