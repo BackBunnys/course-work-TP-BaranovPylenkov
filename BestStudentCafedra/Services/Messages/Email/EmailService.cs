728aa46b38dd1967f1ff5150870749f10eae67c9
Pylenkov Roman <solodov1994roma@gmail.com>
2021-01-20 20:56:46 +0300

    Added EmailService implements EmailSender abstraction

---
+++ BestStudentCafedra/Services/Messages/Email/EmailService.cs
@@ -0,0 +1,34 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestStudentCafedra.Services.Messages.Email
+{
+    public class EmailService: EmailSender
+    {
+        public override async Task SendEmailAsync(List<string> adresses, EmailMessage message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Университет", "login@yandex.ru"));
+            emailMessage.To.AddRange(adresses.Select(x => new MailboxAddress("", x)).ToList());
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message.Content
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                await client.AuthenticateAsync("login@yandex.ru", "password");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
