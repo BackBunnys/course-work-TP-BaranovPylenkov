e02094f932a7d4a0611fbc7ff127864b6f17554d
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2021-01-26 12:21:29 +0300

    Deleted unused methods from controller

--- BestStudentCafedra/Controllers/RatingControlsController.cs
+++ BestStudentCafedra/Controllers/RatingControlsController.cs
@@ -723,58 +723,6 @@ namespace BestStudentCafedra.Controllers
             return builder.ToString();
         }
 
-        public string GetDisplayName(object obj, string propertyName)
-        {
-            if (obj == null) return null;
-            return GetDisplayName(obj.GetType(), propertyName);
-
-        }
-
-        public string GetDisplayName(Type type, string propertyName)
-        {
-            var property = type.GetProperty(propertyName);
-            if (property == null) return null;
-
-            return GetDisplayName(property);
-        }
-
-        public string GetDisplayName(PropertyInfo property)
-        {
-            var attrName = GetAttributeDisplayName(property);
-            if (!string.IsNullOrEmpty(attrName))
-                return attrName;
-
-            var metaName = GetMetaDisplayName(property);
-            if (!string.IsNullOrEmpty(metaName))
-                return metaName;
-
-            return property.Name.ToString();
-        }
-
-        private string GetAttributeDisplayName(PropertyInfo property)
-        {
-            var atts = property.GetCustomAttributes(
-                typeof(DisplayNameAttribute), true);
-            if (atts.Length == 0)
-                return null;
-            return (atts[0] as DisplayNameAttribute).DisplayName;
-        }
-
-        private string GetMetaDisplayName(PropertyInfo property)
-        {
-            var atts = property.DeclaringType.GetCustomAttributes(
-                typeof(MetadataTypeAttribute), true);
-            if (atts.Length == 0)
-                return null;
-
-            var metaAttr = atts[0] as MetadataTypeAttribute;
-            var metaProperty =
-                metaAttr.MetadataClassType.GetProperty(property.Name);
-            if (metaProperty == null)
-                return null;
-            return GetAttributeDisplayName(metaProperty);
-        }
-
         private bool RatingControlExists(int id)
         {
             return _context.RatingControls.Any(e => e.Id == id);
