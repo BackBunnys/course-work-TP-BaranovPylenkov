2b7411ecde8df92af1f2b1a27bf98858bfbc6555
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2021-01-26 12:17:41 +0300

    Added annotation to student rating model

--- BestStudentCafedra/Models/StudentRating.cs
+++ BestStudentCafedra/Models/StudentRating.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace BestStudentCafedra.Models
         public int Id { get; set; }
         public int StudentId { get; set; }
         public int RatingId { get; set; }
+        [Display(Name = "Баллы")]
         public float Points { get; set; }
 
         public virtual Student Student { get; set; }
