c76aa03a5bd8cda337597c06f39892efa6c3bc47
Pylenkov Roman <solodov1994roma@gmail.com>
2021-02-04 01:45:03 +0300

    Added display for roles taghelper

---
+++ BestStudentCafedra/TagHelpers/DisplayForRolesTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace BestStudentCafedra.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "display-for")]
+    public class DisplayForRolesTagHelper : TagHelper
+    {
+        public string DisplayFor { get; set; }
+        
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> roles = new List<string>(DisplayFor.Split(','));
+
+            if (roles.Any(x => ViewContext.HttpContext.User.IsInRole(x.Trim())))
+                return;   
+
+            output.SuppressOutput();
+        }
+    }
+}
