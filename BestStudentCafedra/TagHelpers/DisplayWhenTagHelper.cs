80bf6d1c569d47007644c6c19ae4f90b05e9f851
Pylenkov Roman <solodov1994roma@gmail.com>
2021-02-04 00:27:41 +0300

    Added display when condition is true

---
+++ BestStudentCafedra/TagHelpers/DisplayWhenTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Linq;
+
+namespace BestStudentCafedra.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "display-when")]
+    public class DisplayWhenTagHelper : TagHelper
+    {
+        public bool DisplayWhen { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = context.TagName;
+
+            if (!DisplayWhen)
+                output.SuppressOutput();
+        }
+    }
+}
